c1f20b5be74dd00608007545b5365f7c58d1e793
Jugal <jugaldeepak@gmail.com>
2021-03-06 04:42:52 +0530

    shop initialised

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {
     path: "",
     component: LoginComponent,
+  },
+  {
+    path: "shop",
+    component: ShopComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    ShopComponent,
   ],
   imports: [
     BrowserModule,
