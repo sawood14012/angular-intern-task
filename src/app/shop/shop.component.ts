6143df7d1a17bb88243591a2ca513345975787c8
Jugal <jugaldeepak@gmail.com>
2021-03-06 16:32:50 +0530

    shop ui

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
+  data = [
+    {
+      id: 20,
+      title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+      price: 12.99,
+      description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+      category: 'women clothing',
+      image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg'
+    },
+    {
+      id: 21,
+      title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+      price: 12.99,
+      description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+      category: 'women clothing',
+      image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg'
+    },
+    {
+      id: 22,
+      title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+      price: 12.99,
+      description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+      category: 'women clothing',
+      image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg'
+    },
+    {
+      id: 23,
+      title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+      price: 12.99,
+      description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+      category: 'women clothing',
+      image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
